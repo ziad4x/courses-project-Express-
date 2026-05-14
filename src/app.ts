e34c6f52572c38bb2256bea2ca444078bdd19d3f
ziad4x <zeyad2011am@gmail.com>
2026-05-14 23:11:35 +0300

    fix connection string and dns

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import routes from "./routes";
-import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use("/api", routes);
-app.listen(process.env.PORT ?? 5000, () => {
-    console.log("server started on port " + process.env.PORT);
-});
+
+export default app;
