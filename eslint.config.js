9ad0ebc20a11b57da586ccd0b2c50d6d82cac031
ziad4x <zeyad2011am@gmail.com>
2026-05-14 18:54:34 +0300

    user,category,course models

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,19 @@
-const tselint = require('@typescript-eslint/eslint-plugin');
-const parser = require('@typescript-eslint/parser');
-
+const tselint = require("@typescript-eslint/eslint-plugin");
+const parser = require("@typescript-eslint/parser");
 
 module.exports = [
-    {
-        files: ["**/*.ts"],
-        languageOptions: {
-            parser,
-            parserOptions: {
-                project: "./tsconfig.json",
-                sourceType: "module",
-            },
-        },
-        plugins: {
-            "@typescript-eslint": tselint,
-        },
-        rules: {},
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        // sourceType: "script",
+      },
     },
+    plugins: {
+      "@typescript-eslint": tselint,
+    },
+    rules: {},
+  },
 ];
