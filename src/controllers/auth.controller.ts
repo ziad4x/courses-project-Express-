7e39acb92ff545f6d2052b02dbe544d3cbe54bd6
ziad4x <zeyad2011am@gmail.com>
2026-05-21 20:55:15 +0300

    login & register

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models/user.model";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken";
+const register = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password, type } = req.body;
+        const userExists = await User.findOne({ email })
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const jwtToken = generateToken({ name, email, password: hashedPassword, type })
+        const user = new User({ name, email, password: hashedPassword, type, token: jwtToken })
+        await user.save()
+        res.status(201).json({ message: "User created successfully", data: user })
+
+    }
+    catch (err: any) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+
+}
+const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select("+password")
+        if (!user) {
+            return res.status(401).json({ message: "User not found" })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password" })
+        }
+        const jwtToken = generateToken({ username: user.name, email: user.email, password: user.password, type: user.type })
+        user.token = jwtToken
+        await user.save()
+        const { password: removedPassword, ...rest } = user
+        res.status(200).json({ message: "Login successful", data: rest })
+
+    }
+    catch (err: any) {
+        console.log(err)
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+export default { register, login }
